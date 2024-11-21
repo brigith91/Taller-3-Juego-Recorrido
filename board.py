d2b643f7e32b86204cf47ef0e27e582f88d236fd
brigith91 <168245239+brigith91@users.noreply.github.com>
2024-11-21 23:51:49 +0000

    cambios

--- board.py
+++ board.py
@@ -13,3 +13,15 @@ tablero = (
     [SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE],
     [SPACE, SPACE, SPACE, SPACE, SPACE, SPACE, SPACE],
 )
+
+def leer_tablero(nivel):
+    with open(f'{nivel}.board', 'r') as f:
+        lineas = f.readlines()
+
+        for linea in lines:
+            linea = linea[:-1].strip() # quita el ultimo elemento y el espacio
+            print(linea)
+
+
+if __name__ == '__main__':
+    leer_tablero('nivel_1')
